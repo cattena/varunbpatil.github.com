612123d2dced3810a2b22cecf1baf9e29bfb359e
Varun B Patil <varun.basavaraj.patil@gmail.com>
2013-11-18 18:56:13 +0530

    updated post jekyll build failed

--- publish.sh
+++ publish.sh
@@ -2,11 +2,31 @@
 #
 # script to build website and push it to github
 
+# verify number of arguments
+if [ $# -gt 1 ]; then
+    echo "$(tput setaf 1)Incorrect command usage. Usage : $0 <optional_commit_sha1>$(tput sgr0)"
+    exit 1
+fi
+
+# first argument (if any) is the commit SHA1
+if [ $# -eq 1 ]; then
+    SHA1=$1
+else
+    SHA1="HEAD"
+fi
+echo "$(tput setaf 2)Going to publish commit ${SHA1}$(tput sgr0)"
+
 # create a tmp dir into which jekyll will build the html source
 if [ -d "/tmp/jekyll_build" ]; then
     rm -rf /tmp/jekyll_build
 fi
 mkdir /tmp/jekyll_build
+
+# get a clean copy of the repo(current directory) in /tmp
+cp -r $PWD /tmp/ >&/dev/null
+cd /tmp/varunbpatil.github.com
+git clean -fd >&/dev/null
+git reset --hard $SHA1 >&/dev/null
 
 # build website
 jekyll build -d /tmp/jekyll_build/
@@ -28,3 +48,4 @@ fi
 
 # cleanup
 rm -rf /tmp/jekyll_build
+rm -rf /tmp/varunbpatil.github.com
